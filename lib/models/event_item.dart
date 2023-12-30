bd61709c7eb53b4cdef3599b8f15bb8dbd8e17a2
Chaoward <57738845+Chaoward@users.noreply.github.com>
2023-12-29 18:54:19 -0800

    Added EventItem class

---
+++ lib/models/event_item.dart
@@ -0,0 +1,9 @@
+class EventItem {
+  String name = "";
+  String decription = "";
+  String imageURL = "";
+  DateTime dateTime;
+  String location;
+  
+  EventItem(this.name, this.decription, this.dateTime, this.location, this.imageURL);
+}
