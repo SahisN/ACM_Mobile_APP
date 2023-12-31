66fb70747c8baa5e1b1e2cf83845b1b3791964e4
EricM1111 <ericmwork6097@gmail.com>
2023-12-31 14:12:47 -0800

    Contact page done
    Contact Page now takes the user to linked site

---
+++ lib/data/social_media.dart
@@ -0,0 +1,30 @@
+import 'package:acm_app/model/social_media_type.dart';
+import 'package:flutter/material.dart';
+
+final socialMediaTypes = [
+  SocialMediaType(
+    bgColor: [Colors.black87],
+    imageLogo: 'assets/images/acm_blob.png',
+    url: 'https://acm-calstatela.com',
+  ),
+  SocialMediaType(
+    bgColor: [Colors.blue],
+    imageLogo: 'assets/images/discord.png',
+    url: 'https://discord.com/invite/wX58JRv',
+  ),
+  SocialMediaType(
+    bgColor: [Colors.lightBlueAccent],
+    imageLogo: 'assets/images/linkedin.png',
+    url: 'https://www.linkedin.com/in/acm-cal-state-la-2a92b4260/',
+  ),
+  SocialMediaType(
+    bgColor: [Colors.pink],
+    imageLogo: 'assets/images/instagram.png',
+    url: 'https://www.instagram.com/calstatela_acm/?hl=en',
+  ),
+  SocialMediaType(
+    bgColor: [Colors.teal],
+    imageLogo: 'assets/images/facebook.png',
+    url: 'https://www.facebook.com/calstatela.acm',
+  ),
+];
