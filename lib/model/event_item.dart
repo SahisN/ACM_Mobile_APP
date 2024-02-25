75aed5ee8e15eb81b2cb12decb2acc1bbc7662b2
Chaoward <57738845+Chaoward@users.noreply.github.com>
2024-02-25 13:15:01 -0800

    Added Descript field

--- lib/model/event_item.dart
+++ lib/model/event_item.dart
@@ -3,10 +3,11 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class EventItem {
   String name = "";
   String imageURL = "";
+  String description = "";
   DateTime dateTime;
   String location;
 
-  EventItem(this.name, this.dateTime, this.location, this.imageURL);
+  EventItem(this.name, this.dateTime, this.description, this.location, this.imageURL);
 
   //returns a new EventItem from the passed in event json map;
   factory EventItem.parseJson(Map<String, dynamic> json) {
@@ -16,18 +17,19 @@ class EventItem {
     final imageURL = json['imgUrl'] as String;
     final DateTime dateTime = (json['datetime'] as Timestamp).toDate();
 
-    return EventItem(name, dateTime, location, imageURL);
+    return EventItem(name, dateTime, '', location, imageURL);
   }
 
   factory EventItem.parseJson_googleCal(Map<String, dynamic> json) {
     //type casting
     final name = (json['summary'] ?? "event_summary") as String;
+    final description = (json['description'] ?? "No Description Provided.") as String;
     final location = (json['location'] ?? "somewhere") as String;
     final imageURL = "";
     //print( (json["start"] as Map<String, dynamic>).entries.toString() );
     final DateTime dateTime =
         DateTime.parse(json["start"]["date"] ?? json["start"]["dateTime"]);
 
-    return EventItem(name, dateTime, location, imageURL);
+    return EventItem(name, dateTime, description, location, imageURL);
   }
 }
