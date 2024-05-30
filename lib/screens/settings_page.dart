aa7b9c8f1c49f50ece27ad119de05a6694129020
Sahis <sahisneupane23@gmail.com>
2024-05-29 21:34:54 -0700

    Added settings_tile & update setting page

--- lib/screens/settings_page.dart
+++ lib/screens/settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:acm_app/provider/theme_provider.dart';
+import 'package:acm_app/widget/settings_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -12,78 +13,55 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
-    bool darkMode = Provider.of<ThemeProvider>(context).darkModeSelected;
-    bool notification = false;
+    //bool darkMode = Provider.of<ThemeProvider>(context).darkModeSelected;
+    //bool notification = false;
 
     return Scaffold(
       appBar: AppBar(
         title: const Text('Settings'),
       ),
-      body: Column(
-        children: [
-          Table(
-            columnWidths: const {
-              0: FlexColumnWidth(0.5), // Column width for the Switch
-              1: FlexColumnWidth(), // Column width for the Text
-            },
-            children: [
-              TableRow(
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          'Dark Theme',
-                          style: TextStyle(
-                            fontSize: 32.0,
-                            color: Theme.of(context).colorScheme.inversePrimary,
-                          ), // Font size of the Text
-                        ),
-                      ),
-                      Switch(
-                        value: darkMode,
-                        onChanged: (darkModeSelected) {
-                          setState(() {
-                            darkMode = darkModeSelected;
-                          });
-                          // Code that will control what will the theme
-                          Provider.of<ThemeProvider>(context, listen: false)
-                              .toggleTheme();
-                        },
-                      ),
-                    ],
-                  ),
-                ],
+      body: Container(
+        padding: const EdgeInsets.only(left: 25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(
+              height: 15,
+            ),
+
+            // General settings
+            const Text(
+              'General',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
               ),
-              TableRow(
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          'Notifications',
-                          style: TextStyle(
-                            fontSize: 32.0,
-                            color: Theme.of(context).colorScheme.inversePrimary,
-                          ),
-                        ),
-                      ),
-                      Switch(
-                        value: notification,
-                        onChanged: (isEnabled) {
-                          setState(() {
-                            notification = isEnabled;
-                          });
-                          // Code that will control what will happen when switch is turn on/off
-                        },
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
+            ),
+            SettingsTile(
+              icon: Icons.notifications_outlined,
+              title: 'Notification',
+              onTap: () {
+                // Handle tap
+              },
+            ),
+
+            SettingsTile(
+              icon: Icons.dark_mode_outlined,
+              title: 'Dark Mode',
+              onTap: () {
+                // Handle tap
+              },
+            ),
+
+            SettingsTile(
+              icon: Icons.help_outline,
+              title: 'About Us',
+              onTap: () {
+                // Handle tap
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
