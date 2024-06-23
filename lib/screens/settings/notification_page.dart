046127f4ea8cb9fb68762373d9f6aa1f6bb9b3a0
Sahis <Sahisneupane23@gmail.com>
2024-06-23 12:28:05 -0400

    added time wheel

--- lib/screens/settings/notification_page.dart
+++ lib/screens/settings/notification_page.dart
@@ -1,4 +1,5 @@
-import 'package:acm_app/widget/settings_tile.dart';
+import 'package:acm_app/widget/notification_wheel_picker.dart';
+import 'package:acm_app/widget/settings_switch.dart';
 import 'package:flutter/material.dart';
 
 class NotificationPage extends StatelessWidget {
@@ -21,10 +22,11 @@ class NotificationPage extends StatelessWidget {
               ),
 
               // enable/disable notification option
-              SettingsTile(
+              SettingsSwitch(
                 icon: Icons.notifications_outlined,
                 title: 'Allow Notification',
-                onTap: () {},
+                value: true,
+                onChanged: (bool allowNotification) {},
               ),
 
               const SizedBox(
@@ -32,13 +34,69 @@ class NotificationPage extends StatelessWidget {
               ),
 
               // early reminder customization
-              Text(
-                'Early Reminder Customization',
-                style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).colorScheme.inversePrimary),
+              Center(
+                child: Text(
+                  'Set Early Reminders',
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.inversePrimary),
+                ),
               ),
+              const SizedBox(height: 20),
+              Row(
+                children: [
+                  Text(
+                    'Days',
+                    style: TextStyle(
+                        fontSize: 22,
+                        color: Theme.of(context).colorScheme.inversePrimary),
+                  ),
+                  const SizedBox(width: 60),
+                  Text(
+                    'Hours',
+                    style: TextStyle(
+                        fontSize: 22,
+                        color: Theme.of(context).colorScheme.inversePrimary),
+                  ),
+                  const SizedBox(width: 60),
+                  Text(
+                    'Minutes',
+                    style: TextStyle(
+                        fontSize: 22,
+                        color: Theme.of(context).colorScheme.inversePrimary),
+                  ),
+                ],
+              ),
+              // time wheel
+              Row(
+                children: [
+                  // days wheel
+                  NotificationWheelPicker(
+                    range: 5,
+                    initialValue: 0,
+                    onChanged: (int newDay) {},
+                  ),
+                  const SizedBox(width: 75),
+
+                  // hours wheel
+                  NotificationWheelPicker(
+                    range: 12,
+                    initialValue: 0,
+                    onChanged: (int newHour) {},
+                  ),
+                  const SizedBox(
+                    width: 80,
+                  ),
+
+                  // minutes wheel
+                  NotificationWheelPicker(
+                    range: 60,
+                    initialValue: 0,
+                    onChanged: (int newMinute) {},
+                  ),
+                ],
+              )
             ],
           ),
         ),
